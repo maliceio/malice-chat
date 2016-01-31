2742d8b92865210aac1d3f55323537583dcf9318
blacktop <blacktop@users.noreply.github.com>
2016-01-30 19:13:47 -0700

    add docker stuff

---
+++ api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	r "github.com/dancannon/gorethink"
+)
+
+func main() {
+	session, err := r.Connect(r.ConnectOpts{
+		Address:  "rethinkdb:28015",
+		Database: "malice",
+	})
+
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	router := NewRouter(session)
+
+	router.Handle("channel add", addChannel)
+	router.Handle("channel subscribe", subscribeChannel)
+	router.Handle("channel unsubscribe", unsubscribeChannel)
+
+	router.Handle("user edit", editUser)
+	router.Handle("user subscribe", subscribeUser)
+	router.Handle("user unsubscribe", unsubscribeUser)
+
+	router.Handle("message add", addChannelMessage)
+	router.Handle("message subscribe", subscribeChannelMessage)
+	router.Handle("message unsubscribe", unsubscribeChannelMessage)
+	http.Handle("/", router)
+	http.ListenAndServe(":4000", nil)
+}
--- api/router.go
+++ api/router.go
@@ -8,7 +8,6 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// Handler is a handler
 type Handler func(*Client, interface{})
 
 var upgrader = websocket.Upgrader{
@@ -17,21 +16,11 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
-// Router is router
 type Router struct {
 	rules   map[string]Handler
 	session *r.Session
 }
 
-func handleError(socket *websocket.Conn, err error) {
-	var outMessage Message
-	if err != nil {
-		outMessage = Message{Name: "error", Data: err}
-		assert(socket.WriteJSON(outMessage))
-	}
-}
-
-// NewRouter creates a new Router
 func NewRouter(session *r.Session) *Router {
 	return &Router{
 		rules:   make(map[string]Handler),
@@ -39,26 +28,25 @@ func NewRouter(session *r.Session) *Router {
 	}
 }
 
-// Handle handles thangs
-func (rout *Router) Handle(msgName string, handler Handler) {
-	rout.rules[msgName] = handler
+func (r *Router) Handle(msgName string, handler Handler) {
+	r.rules[msgName] = handler
 }
 
-// FindHandler finds handler
-func (rout *Router) FindHandler(msgName string) (Handler, bool) {
-	handler, found := rout.rules[msgName]
+func (r *Router) FindHandler(msgName string) (Handler, bool) {
+	handler, found := r.rules[msgName]
 	return handler, found
 }
 
-// ServeHTTP serves it up fresh
-func (rout *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (e *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	socket, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, err.Error())
 		return
 	}
-	client := NewClient(socket, rout.FindHandler, rout.session)
+	client := NewClient(socket, e.FindHandler, e.session)
+	defer client.Close()
 	go client.Write()
 	client.Read()
+
 }
